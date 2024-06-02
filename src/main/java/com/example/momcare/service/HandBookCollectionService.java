43294af65fc3b0db05d75d0da3918efbbc2a5103
MeanIsme <20110524@student.hcmute.edu.vn>
2024-06-02 10:55:53 +0700

    Refactor code HandBookCollection

--- src/main/java/com/example/momcare/service/HandBookCollectionService.java
+++ src/main/java/com/example/momcare/service/HandBookCollectionService.java
@@ -1,7 +1,9 @@
 package com.example.momcare.service;
 
 import com.example.momcare.models.HandBookCollection;
+import com.example.momcare.payload.response.Response;
 import com.example.momcare.repository.HandBookCollectionRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,5 +16,9 @@ public class HandBookCollectionService {
         this.repository = repository;
     }
 
+    public Response findAllCollection() {
+        List<HandBookCollection> diaries = repository.findAll();
+        return new Response(HttpStatus.OK.getReasonPhrase(), diaries, "success");
+    }
     public List<HandBookCollection> findAll(){ return this.repository.findAll();}
 }
