f7dfa4756f3adc5e9e2236d4068572cd1b1e704f
MeanIsme <20110524@student.hcmute.edu.vn>
2024-06-02 11:51:17 +0700

    Refactor code Music

--- src/main/java/com/example/momcare/controllers/MusicController.java
+++ src/main/java/com/example/momcare/controllers/MusicController.java
@@ -1,32 +1,31 @@
 package com.example.momcare.controllers;
 
-import com.example.momcare.models.Category;
 import com.example.momcare.payload.response.Response;
-import com.example.momcare.repository.MusicRepository;
 import com.example.momcare.service.MusicService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MusicController {
-    @Autowired
+
     MusicService musicService;
 
+    public MusicController(MusicService musicService) {
+        this.musicService = musicService;
+    }
+
     @GetMapping("music/random")
-    public Response GetRandomMusic(){
-        return new Response((HttpStatus.OK.getReasonPhrase()), musicService.Top8Random(), "success");
+    public Response getRandomMusic(){
+        return musicService.getRandomMusic();
     }
     @GetMapping("music/category")
-    public Response GetMusicByCategory(@RequestParam String category){
-        return new Response((HttpStatus.OK.getReasonPhrase()), musicService.findMusicByCategory(category), "success");
+    public Response getMusicByCategory(@RequestParam String category){
+        return musicService.getMusicByCategory(category);
     }
     @GetMapping("music/allcategory")
-    public Response GetCategory(){
-        return new Response((HttpStatus.OK.getReasonPhrase()), musicService.getAllCategories(), "success");
+    public Response getCategory(){
+        return musicService.getCategory();
     }
 }
--- src/main/java/com/example/momcare/service/MusicService.java
+++ src/main/java/com/example/momcare/service/MusicService.java
@@ -1,10 +1,12 @@
 package com.example.momcare.service;
 
-import com.example.momcare.models.Category;
 import com.example.momcare.models.Music;
 import com.example.momcare.models.MusicCategory;
+import com.example.momcare.payload.response.Response;
 import com.example.momcare.repository.MusicCategoryRepository;
 import com.example.momcare.repository.MusicRepository;
+import com.example.momcare.util.Constant;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -20,8 +22,17 @@ public class MusicService {
         this.musicRepository = musicRepository;
         this.musicCategoryRepository = musicCategoryRepository;
     }
+    public Response getRandomMusic(){
+        return new Response((HttpStatus.OK.getReasonPhrase()), top8Random(), Constant.SUCCESS);
+    }
+    public Response getMusicByCategory(String category){
+        return new Response((HttpStatus.OK.getReasonPhrase()), findMusicByCategory(category), Constant.SUCCESS);
+    }
+    public Response getCategory(){
+        return new Response((HttpStatus.OK.getReasonPhrase()), getAllCategories(), Constant.SUCCESS);
+    }
 
-    public List<Music> Top8Random() {
+    public List<Music> top8Random() {
         List<Music> allMusic= musicRepository.findAll();
         int totalMusic = allMusic.size();
         if (totalMusic <= 8) {
