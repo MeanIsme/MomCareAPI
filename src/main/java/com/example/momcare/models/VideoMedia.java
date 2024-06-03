5085b950cb7274fee873ae18d9eb8fd8645e50d4
MeanIsme <20110524@student.hcmute.edu.vn>
2024-06-03 17:31:25 +0700

    fix bug media

--- src/main/java/com/example/momcare/models/VideoMedia.java
+++ src/main/java/com/example/momcare/models/VideoMedia.java
@@ -9,9 +9,5 @@ public class VideoMedia extends Media{
     private double duration;
     private String resolution;
 
-    public VideoMedia(String url, String ration, String description, int reactCount, int commentCount, int shareCount, double duration, String resolution) {
-        super(url, ration, description, reactCount, commentCount, shareCount);
-        this.duration = duration;
-        this.resolution = resolution;
-    }
+
 }
